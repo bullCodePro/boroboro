bedb86284a57fd4fae8ce870a2f417677ee0696f
bullodepro <bullodepro@gmail.com>
2025-11-21 15:34:50 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const { App } = require("@slack/bolt");
 
 const app = new App({
-token: "N08w9wwFukM2CNYjYMqON5SH", 
-signingSecret: "627c04c0d75d35c8eb7d8fd648c5b8aa"
+token: process.env.SLACK_BOT_TOKEN,
+signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 function getNextWorkdayTarget() {
